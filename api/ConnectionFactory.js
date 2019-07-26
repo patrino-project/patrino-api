192201625e7dc6b031da0c0bd4f27f2d9b36d8cb
Eduardo Vieira <edusvieirap@gmail.com>
2019-07-26 20:32:13 -0300

    Refatorando a classe

--- api/ConnectionFactory.js
+++ api/ConnectionFactory.js
@@ -1,37 +1,12 @@
 var mysql = require('mysql');
 
-module.exports = class ConnectionFactory {
-    /*Criando conexão com o database*/
-    open() {
-        var connection = mysql.createConnection({
-          host: "localhost",
-          user: "root",
-          password: "eduardo",
-          database: "patrinodb"
-        });
+module.exports.getConnection = function() {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mysql",
+    database: "patrinodb"
+  });
 
-        connection.connect(function(err) {
-          if (err) throw err;
-          console.log("Você criou uma nova conexão com o banco de dados.");
-          return connection
-        });
-    }
-
-    read() {
-        var connection = mysql.createConnection({
-          host: "localhost",
-          user: "root",
-          password: "eduardo",
-          database: "patrinodb"
-        });
-
-        connection.connect(function(err) {
-          if (err) throw err;
-          var sql = "SELECT * FROM users";
-          connection.query(sql, function (err, result) {
-            if (err) throw err;
-            return result;
-          });
-        });
-    }
+  return connection;
 }
---
+++ api/MotherModel.js
@@ -0,0 +1,100 @@
+var ConnectionFactory = require("./ConnectionFactory.js");
+
+/*Obtendo os usuários cadastrados no database*/
+module.exports = function(app) {
+  app.get('/users/', function(req, res) {
+      var connection = ConnectionFactory.getConnection();
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        var sql = "SELECT * FROM mothers";
+        connection.query(sql, function (err, result, fields) {
+          if (err) throw err;
+
+          console.log(result);
+
+          res.send(JSON.stringify(result));
+        });
+      });
+  });
+
+  /*Realizando login na aplicação*/
+  app.post("/login/", function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var connection = ConnectionFactory.getConnection();
+
+    connection.connect(function(err) {
+      if (err) throw err;
+      var sql = "SELECT * FROM mothers WHERE email='" + email + "' AND password='" + password + "'";
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+
+        if(result.length > 0) {
+          console.log("Usuário autenticado!");
+
+          res.send(JSON.stringify({"message": "OK", "code": result[0].code }));
+        } else {
+          console.log("Usuário não autenticado!");
+
+          res.send(JSON.stringify({"message": "ERROR"}));
+        }
+      });
+    });
+
+  });
+
+  /*Criando um novo usuário*/
+  app.post('/users/', function(req, res) {
+      var name = req.body.name;
+      var email = req.body.email;
+      var password = req.body.password;
+      var telefone = req.body.phone;
+      var endereco = req.body.address;
+      var createdAt = new Date().toLocaleDateString();
+      var modifiedAt = new Date().toLocaleDateString();
+
+      var connection = ConnectionFactory.getConnection();
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        var sql = "INSERT INTO mothers(name, email, password, telefone, endereco) VALUES('" + name + "', '" + email + "', '" + password + "', '" + telefone + "', '" + endereco + "')";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Usuário inserido com sucesso!");
+
+          res.send(JSON.stringify({"message": "OK"}));
+        });
+      });
+
+  });
+
+
+  /*Atualizando usuário*/
+  app.put('/users/', function(req, res) {
+      var code = req.body.code;
+      var name = req.body.name;
+      var email = req.body.email;
+      var password = req.body.password;
+      var telefone = req.body.phone;
+      var endereco = req.body.address;
+      var createdAt = new Date().toLocaleDateString();
+      var modifiedAt = new Date().toLocaleDateString();
+
+      var connection = ConnectionFactory.getConnection();
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        var sql = "UPDATE mothers SET name='" + name + "', email='" + email + "', password='" + password + "', telefone='" + telefone + "', endereco='" + endereco + "' WHERE code='" + code + "'";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Usuário atualizado com sucesso!");
+
+          res.send(JSON.stringify({"message": "OK"}));
+        });
+      });
+
+  });
+
+}
